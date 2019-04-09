ee0010aeb355723a25709aaff32cd1ceb5564135
Takuya Kikuchi <takuyakikuchi@TakuyanoMac-mini.local>
2019-04-09 18:45:49 +0900

    create class component and finction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-     const greeting = "Hi,Tom!";
-     const dom = <h1>{greeting}</h1>;
-    return dom;
-  }
+//class App extends Component {
+  //render() {
+    // const greeting = "Hi,Tom!";
+     //const dom = <h1>{greeting}</h1>;
+    //return dom;
+  //}
+//}
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
