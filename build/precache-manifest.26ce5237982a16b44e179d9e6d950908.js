094831656f502aac8f01136fde814c3618760d5d
Takuya Kikuchi <takuyakikuchi@TakuyanoMac-mini.local>
2019-04-09 14:40:56 +0900

    learn jsx sntax

---
+++ build/precache-manifest.26ce5237982a16b44e179d9e6d950908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7892ff516c1ab702356e",
+    "url": "/static/js/main.21feb230.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "7892ff516c1ab702356e",
+    "url": "/static/css/main.627cc665.chunk.css"
+  },
+  {
+    "revision": "e529454b87d741926c7d35d2f04797c9",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+     const greeting = "Hi,Tom!";
+     const dom = <h1>{greeting}</h1>;
+    return dom;
   }
 }
 
