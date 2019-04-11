dc3f7353c9520af64430e5d981cc8293b7e524df
Takuya Kikuchi <takuyakikuchi@TakuyanoMac-mini.local>
2019-04-11 20:50:29 +0900

    fix

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,6 +1,6 @@
 import { INCREMENT, DECREMENT} from '../actions'
 
-const initialState = { valur: 0}
+const initialState = { value: 0 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
